c6b17fb2938faf24b5832a503c3274eb3c2fcd9a
Andi Altendorfer <andi@iboard.cc>
2012-10-29 11:31:52 +0100

    Save original requested path before redirecting to sign-in

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -13,6 +13,7 @@ module Admin
     private
     def authenticate_admin!
       unless current_user && current_user.is_admin?
+        session[:requested_path] = request.original_fullpath
         redirect_to main_app.new_session_path, alert: t('admin.action.needs_sign_in', action: "Admin")
       end
     end
